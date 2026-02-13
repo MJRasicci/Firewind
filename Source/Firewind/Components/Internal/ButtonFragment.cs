30ebf754eccec6e7eb329f6f901f6ebeeabdd5bd
Michael Rasicci <mike@mjrasicci.dev>
2026-02-13 00:01:26 -0500

    Remove unused code

--- Source/Firewind/Components/Internal/ButtonFragment.cs
+++
@@ -1,8 +0,0 @@
-namespace Firewind.Components.Internal;
-
-/// <summary>
-/// Represents a button component intended for fragment-style composition scenarios.
-/// </summary>
-public sealed class ButtonFragment : FWButton
-{
-}
