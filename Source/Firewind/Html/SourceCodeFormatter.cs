28782bdc637b00cfe38fab3aa4a91b63a7d28842
Michael Rasicci <mike@mjrasicci.dev>
2026-02-12 22:58:37 -0500

    Wire codeview syntax highlighting
    FWCodeView now uses SourceCodeFormatter and the Language parameter to render syntax-highlighted markup instead of plain text lines.\n\nSourceCodeFormatter was rewritten to use ColorCode's HtmlFormatter per line and return line-numbered <pre> blocks, with plain-text fallback when language resolution fails.

--- Source/Firewind/Html/SourceCodeFormatter.cs
+++ Source/Firewind/Html/SourceCodeFormatter.cs
@@ -1,118 +1,59 @@
 namespace Firewind.Html;
 
 using ColorCode;
-using ColorCode.Common;
-using ColorCode.Parsing;
 using ColorCode.Styling;
 using Microsoft.AspNetCore.Components;
 using System.Net;
 using System.Text;
 
-internal class SourceCodeFormatter : CodeColorizerBase
+internal sealed class SourceCodeFormatter
 {
-    private TextWriter writer = null!;
-    private int lineNumber = 1;
-    private static readonly string[] separator = ["\r\n", "\r", "\n"];
+    private static readonly string[] LineSeparators = ["\r\n", "\n", "\r"];
+    private readonly HtmlFormatter formatter;
 
-    public SourceCodeFormatter(StyleDictionary? style = null, ILanguageParser? languageParser = null)
-        : base(style, languageParser)
-    { }
+    public SourceCodeFormatter(StyleDictionary? style = null)
+        => this.formatter = new HtmlFormatter(style ?? StyleDictionary.DefaultDark, languageParser: null);
 
     public MarkupString GetMarkupString(string sourceCode, ILanguage language)
     {
-        this.lineNumber = 1;
-        var buffer = new StringBuilder();
+        ArgumentNullException.ThrowIfNull(sourceCode);
+        ArgumentNullException.ThrowIfNull(language);
 
-        using (this.writer = new StringWriter(buffer))
+        var buffer = new StringBuilder();
+        var lines = sourceCode.Split(LineSeparators, StringSplitOptions.None);
+
+        for (var i = 0; i < lines.Length; i++)
         {
-            this.languageParser.Parse(sourceCode, language, Write);
-            this.writer.Flush();
+            buffer.Append("<pre data-prefix=\"")
+                  .Append(i + 1)
+                  .Append("\"><code>")
+                  .Append(this.GetLineMarkup(lines[i], language))
+                  .Append("</code></pre>");
         }
 
         return new MarkupString(buffer.ToString());
     }
 
-    protected override void Write(string parsedSourceCode, IList<Scope> scopes)
+    private string GetLineMarkup(string line, ILanguage language)
     {
-        var styleInsertions = new List<TextInsertion>();
-
-        foreach (var scope in scopes)
+        if (string.IsNullOrEmpty(line))
         {
-            GetStyleInsertionsForCapturedStyle(scope, styleInsertions);
+            return string.Empty;
         }
 
-        styleInsertions.SortStable((x, y) => x.Index.CompareTo(y.Index));
+        const string preOpenTag = "<pre>";
+        const string preCloseTag = "</pre>";
 
-        var offset = 0;
-
-        foreach (var styleInsertion in styleInsertions)
+        var html = this.formatter.GetHtmlString(line, language);
+        var preStart = html.IndexOf(preOpenTag, StringComparison.OrdinalIgnoreCase);
+        var preEnd = html.IndexOf(preCloseTag, StringComparison.OrdinalIgnoreCase);
+        if (preStart < 0 || preEnd <= preStart)
         {
-            WriteHtmlEncodedText(parsedSourceCode[offset..styleInsertion.Index]);
-
-            if (string.IsNullOrEmpty(styleInsertion.Text))
-                BuildSpanForCapturedStyle(styleInsertion.Scope);
-            else
-                this.writer.Write(styleInsertion.Text);
-
-            offset = styleInsertion.Index;
+            return WebUtility.HtmlEncode(line);
         }
 
-        WriteHtmlEncodedText(parsedSourceCode[offset..]);
-    }
-
-    private void WriteHtmlEncodedText(string text)
-    {
-        var encodedText = WebUtility.HtmlEncode(text);
-        var lines = encodedText.Split(separator, StringSplitOptions.RemoveEmptyEntries);
-
-        for (var i = 0; i < lines.Length; i++)
-        {
-            if (i > 0)
-            {
-                this.writer.Write("</code></pre>"); // Close previous tags
-                lineNumber++;
-            }
-
-            this.writer.Write($"<pre data-prefix=\"{lineNumber}\"><code>");
-            this.writer.Write(lines[i]);
-            if (i == lines.Length - 1)
-            {
-                this.writer.Write("</code></pre>"); // Close tags at the end
-            }
-        }
-    }
-
-    private void BuildSpanForCapturedStyle(Scope scope)
-    {
-        var cssClassName = "";
-
-        if (this.Styles.Contains(scope.Name))
-        {
-            var style = this.Styles[scope.Name];
-
-            cssClassName = style.ReferenceName;
-        }
-
-        this.writer.Write($"<span class=\"{cssClassName}\">");
-    }
-
-    private static void GetStyleInsertionsForCapturedStyle(Scope scope, ICollection<TextInsertion> styleInsertions)
-    {
-        styleInsertions.Add(new TextInsertion
-        {
-            Index = scope.Index,
-            Scope = scope
-        });
-
-        foreach (var childScope in scope.Children)
-        {
-            GetStyleInsertionsForCapturedStyle(childScope, styleInsertions);
-        }
-
-        styleInsertions.Add(new TextInsertion
-        {
-            Index = scope.Index + scope.Length,
-            Text = "</span>"
-        });
+        var contentStart = preStart + preOpenTag.Length;
+        var contentLength = preEnd - contentStart;
+        return html.Substring(contentStart, contentLength).Trim('\r', '\n');
     }
 }
