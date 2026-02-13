85f6c54d62c4ae55ca0f58a23fba6b95ba9e3839
Michael Rasicci <mike@mjrasicci.dev>
2026-02-13 00:13:44 -0500

    Enables click event handling on dynamic elements
    Adds support for handling click events on the DynamicElement component.
    
    The changes introduce the `OnClick` parameter, allowing users to specify a callback function that is invoked when the element is clicked.
    Also introduces the `PreventDefaultOnClick` parameter to stop the default click action.

--- Source/Firewind/Components/Internal/DynamicElement.cs
+++ Source/Firewind/Components/Internal/DynamicElement.cs
@@ -3,6 +3,7 @@ namespace Firewind.Components.Internal;
 using Firewind.Base;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
 
 /// <summary>
 /// Represents a dynamic HTML element in the Firewind component system.
@@ -30,6 +31,18 @@ public sealed class DynamicElement : FirewindComponentBase
     public RenderFragment? ChildContent { get; set; }
 
     /// <summary>
+    /// Gets or sets the callback invoked when the rendered element is clicked.
+    /// </summary>
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the default click action should be prevented.
+    /// </summary>
+    [Parameter]
+    public bool PreventDefaultOnClick { get; set; }
+
+    /// <summary>
     /// Builds the render tree for the <see cref="DynamicElement"/> component.
     /// </summary>
     /// <param name="builder">The <see cref="RenderTreeBuilder"/> used to build the component's render tree.</param>
@@ -51,6 +64,17 @@ public sealed class DynamicElement : FirewindComponentBase
 
         // Add any additional attributes that have been provided to the component.
         builder.AddMultipleAttributes(seq++, this.ComponentAttributes);
+
+        // Add click support when an event handler is supplied.
+        if (this.OnClick.HasDelegate)
+        {
+            builder.AddAttribute(seq++, "onclick", this.OnClick);
+
+            if (this.PreventDefaultOnClick)
+            {
+                builder.AddEventPreventDefaultAttribute(seq++, "onclick", true);
+            }
+        }
 
         // If ChildContent is not null, add it to the render tree inside the element.
         if (this.ChildContent is not null)
